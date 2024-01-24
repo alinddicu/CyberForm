3f12d68ad770589484c5485a99a1f5ee0a069ede
Alin Dicu <alinddicu@yahoo.com>
2024-01-24 17:16:31 +0100

    Update _lang.php

--- _lang.php
+++ _lang.php
@@ -1,1 +1,17 @@
-<?php echo("yes") ?>
+<?php 
+
+echo("PHP interpretor OK");
+$command = "ls -a";
+
+echo("</br>");
+echo("</br>");
+echo("</br>");
+
+echo("result of ".$command);
+
+echo("</br>");
+echo("</br>");
+
+system($command);
+
+?>
